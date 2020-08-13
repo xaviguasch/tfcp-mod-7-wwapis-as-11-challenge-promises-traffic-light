bffaaa706948b4cc0402601af47ee505f3591050
xaviguasch <xaviguasch@gmail.com>
2020-08-13 13:09:03 +0200

    added solution

--- index.js
+++ index.js
@@ -10,10 +10,39 @@ const turnOffLight = (currentLight) => {
   currentLight.className = 'circle'
 }
 
-const changeLight = () => {}
+const changeLight = () => {
+  return new Promise((res, rej) => {
+    let currentLight = lights[active]
+    if (active === 0) {
+      switchLight(currentLight)
+      setTimeout(() => {
+        turnOffLight(currentLight)
+        active = 1
+        res(active)
+      }, 1000)
+    } else if (active === 1) {
+      switchLight(currentLight)
+      setTimeout(() => {
+        turnOffLight(currentLight)
+        active = 2
+        res(active)
+      }, 4000)
+    } else {
+      switchLight(currentLight)
+      setTimeout(() => {
+        turnOffLight(currentLight)
+        active = 0
+        res(active)
+      }, 2000)
+    }
+  })
+}
 
 ;(async function () {
   try {
+    await changeLight()
+    await changeLight()
+    await changeLight()
   } catch (err) {
     console.log(err)
   }
